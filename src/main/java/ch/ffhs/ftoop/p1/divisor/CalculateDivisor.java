1e8532b3d0c33ede903f20eca93c628ba3562521
bnc-ricardo <ricardo.manuel@bluewin.ch>
2018-03-15 21:22:38 +0100

    Entwurfsumsetzung

--- src/main/java/ch/ffhs/ftoop/p1/divisor/CalculateDivisor.java
+++ src/main/java/ch/ffhs/ftoop/p1/divisor/CalculateDivisor.java
@@ -1,106 +1,150 @@
 package ch.ffhs.ftoop.p1.divisor;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Das folgende Programm soll aus einem vorgegebene Interval von Long-Zahlen die
  * Zahl zurückgeben, die die meisten Divisoren hat. Sollte es mehrere solche
  * Zahlen geben, so soll die kleinste dieser Zahlen ausgegeben werden.
- * 
+ * <p>
  * Die Berechnung soll in n Threads stattfinden, die via Executor Framework
  * gesteuert werden, und sich das Problem aufteilen - jeder Thread soll eine
  * Teilmenge des Problems lösen. Verwenden Sie bitte einen FixedThreadPool und
  * implementieren Sie die Worker als Callable.
- * 
+ *
  * @author ble
- * 
  */
 public class CalculateDivisor {
 
-	long von, bis;
-	int threadCount;
+    private long von, bis;
+    private int threadCount;
 
-	/**
-	 * @param von
-	 *            untere Intervallgrenze
-	 * @param bis
-	 *            obere Intervallgrenze
-	 * @param threadCount
-	 *            Abzahl der Threads, auf die das Problem aufgeteilt werden soll
-	 */
-	public CalculateDivisor(long von, long bis, int threadCount) {
-		this.von = von;
-		this.bis = bis;
-		this.threadCount = threadCount;
+    /**
+     * @param von         untere Intervallgrenze
+     * @param bis         obere Intervallgrenze
+     * @param threadCount Abzahl der Threads, auf die das Problem aufgeteilt werden soll
+     */
+    public CalculateDivisor(long von, long bis, int threadCount) {
+        this.von = von;
+        this.bis = bis;
+        this.threadCount = threadCount;
 
-	}
+    }
 
-	/**
-	 * Berechnungsroutine
-	 * 
-	 * @return
-	 * @throws InterruptedException
-	 * @throws ExecutionException
-	 */
-	DivisorResult calculate() throws InterruptedException, ExecutionException {
+    /**
+     * Berechnungsroutine
+     *
+     * @return
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    DivisorResult calculate() throws InterruptedException, ExecutionException {
+        List<Future<DivisorResult>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
 
-		// TODO implementieren Sie hier die Logic der calculate Methode
+        for (long dividend = von; dividend <= bis; dividend++) {
+            futures.add(executor.submit(new DivisorTask(dividend)));
+        }
 
-		return new DivisorResult(0, 0);
-	}
+        while (futures.stream().filter(f -> !f.isDone()).count() > 0) {
+            Thread.sleep(500);
+        }
 
-	public static void main(String[] args) throws InterruptedException,
-			ExecutionException {
-		if (args.length != 3) {
-			System.out
-					.println("Usage: CalculateDivisor <intervalStart> <intervalEnd> <threadCount>");
-			System.exit(1);
-		}
-		long von = Long.parseLong(args[0]);
-		long bis = Long.parseLong(args[1]);
-		int threads = Integer.parseInt(args[2]);
+        return futures.stream() //
+                .map(f -> toDivisorResult(f)) //
+                .sorted((o1, o2) -> {
+                    if (o1.countDiv > o2.countDiv) {
+                        return -1;
+                    }
 
-		CalculateDivisor cp = new CalculateDivisor(von, bis, threads);
-		System.out.println("Ergebnis: " + cp.calculate());
-	}
+                    if (o1.countDiv < o2.countDiv) {
+                        return 1;
+                    }
 
+                    return 0;
+
+                }) //
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("Not found"));
+    }
+
+    private DivisorResult toDivisorResult(Future<DivisorResult> future) {
+        try {
+            return future.get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException,
+            ExecutionException {
+        if (args.length != 3) {
+            System.out
+                    .println("Usage: CalculateDivisor <intervalStart> <intervalEnd> <threadCount>");
+            System.exit(1);
+        }
+        long von = Long.parseLong(args[0]);
+        long bis = Long.parseLong(args[1]);
+        int threads = Integer.parseInt(args[2]);
+
+        CalculateDivisor cp = new CalculateDivisor(von, bis, threads);
+        System.out.println("Ergebnis: " + cp.calculate());
+        System.exit(0);
+    }
+
+}
+
+class DivisorTask implements Callable<DivisorResult> {
+
+    final private long dividend;
+    private long divisorCount = 0;
+
+    public DivisorTask(long dividend) {
+        this.dividend = dividend;
+    }
+
+    @Override
+    public DivisorResult call() throws Exception {
+        for (long i = 1; i <= dividend; i++) {
+            if (dividend % i == 0) {
+                divisorCount++;
+            }
+        }
+
+        return new DivisorResult(dividend, divisorCount);
+    }
 }
 
 /**
  * Hält das Ergebnis einer Berechnung
- * 
+ *
  * @author bele
- * 
- * 
  */
 class DivisorResult {
-	// das eigentlich ergebnis - die Zahl mit der max. Anzahl von Divisoren
-	long result;
+    // das eigentlich ergebnis - die Zahl mit der max. Anzahl von Divisoren
+    long result;
 
-	// Anzahl der Divisoren von Result
-	long countDiv;
+    // Anzahl der Divisoren von Result
+    long countDiv;
 
-	public DivisorResult(long r, long c) {
-		result = r;
-		countDiv = c;
-	}
+    public DivisorResult(long r, long c) {
+        result = r;
+        countDiv = c;
+    }
 
-	public long getResult() {
-		return result;
-	}
+    public long getResult() {
+        return result;
+    }
 
-	public long getCountDiv() {
-		return countDiv;
-	}
+    public long getCountDiv() {
+        return countDiv;
+    }
 
-	@Override
-	public String toString() {
-		return "Zahl mit maximaler Anzahl Divisoren: " + result + " ("
-				+ countDiv + " Divisoren)";
-	}
+    @Override
+    public String toString() {
+        return "Zahl mit maximaler Anzahl Divisoren: " + result + " ("
+                + countDiv + " Divisoren)";
+    }
 
 }
